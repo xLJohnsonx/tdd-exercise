44863fbfdfbd847dd9265057b5a5efbbde620a83
CheezItMan <chris@adadevelopersacademy.org>
2018-08-11 22:32:45 -0700

    updated blackjack_score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -3,34 +3,5 @@
 VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
 
 def blackjack_score(hand)
-  score = 0
-  number_aces = 0
 
-  if hand.class != Array || hand.length < 1 || hand.length > 5
-    raise ArgumentError, 'You need to send an array of 1-5 cards'
-  end
-
-  score = hand.sum do |card|
-    unless VALID_CARDS.include? card
-      raise ArgumentError, 'Invalid Card'
-    end
-
-    if card == 1
-      number_aces += 1
-      1
-    elsif card.numeric?
-      card
-    else
-      10
-    end
-  end
-
-  if score > 21
-    raise ArgumentError, 'Busted!'
-  end
-
-  while score <= 11 && number_aces > 0
-    number_aces -= 1
-    score += 10
-  end
 end
